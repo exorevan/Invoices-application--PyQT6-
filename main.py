d16e532e39335a983c1c3b8628c2e40fceab38b5
Khassan Abdallakh <86018711+exorevan@users.noreply.github.com>
2022-08-15 20:00:52 +0300

    Start on designing and first functions
    Update design for page Reports and Invoices
    
    Added a switch between page Invoice and page Invoices
    
    Filling the page Invoices with invoices in set dates

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sqlite3
 import sys
 
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import *
 from PyQt6.uic import loadUi
@@ -9,19 +12,62 @@ class Invoices(QDialog):
     def __init__(self):
         super(Invoices, self).__init__()
         loadUi("Invoices.ui", self)
+        self.newInvoiceButton.clicked.connect(self.makeNewInvoice)
+        self.showInvoicesButton.clicked.connect(self.showInvocies)
+
+    def makeNewInvoice(self):
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        # invoice = Invoice()
+        # widget.setCurrentWidget(invoice)
+
+    def showInvocies(self):
+        date1 = self.datePicker1.date().toString('yyyy-MM-dd')
+        date2 = self.datePicker2.date().toString('yyyy-MM-dd')
+
+        select = "select date, invoice_name as 'Invoice Name', goods, earning from invoices where date between '" + date1 + "' and '" + date2 + "'"
+        result = dbutil.select(select)
+
+        self.tableWidget.setRowCount(len(result))
+
+        for i in range(len(result)):
+            for j in range(4):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(result[i][j]))
+
+        a = 10
 
 
 class Invoice(QDialog):
     def __init__(self):
         super(Invoice, self).__init__()
         loadUi("Invoice.ui", self)
+        self.goBackToInvoicesButton.clicked.connect(self.goBackToInvoices)
+
+    def goBackToInvoices(self):
+        widget.setCurrentIndex(widget.currentIndex() - 1)
 
 
+class DBUtil():
+    def __init__(self):
+        self.connection = sqlite3.connect('db/Accounting.db')
+        self.cursor = self.connection.cursor()
+
+    def select(self, select):
+        self.cursor.execute(select)
+
+        return self.cursor.fetchall()
+
+
+dbutil = DBUtil()
 app = QApplication(sys.argv)
-mainwindow = Invoices()
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
+
+invoices = Invoices()
+widget.addWidget(invoices)
+invoice = Invoice()
+widget.addWidget(invoice)
+
 widget.setFixedWidth(960)
 widget.setFixedHeight(560)
 widget.show()
 sys.exit(app.exec())
+dbutil.connection.close()
